09d2f75a61542b26904ac9f77da9cd5089868505
Varun Ganapathi <varun@cloudlabs.io>
2017-10-13 02:20:22 -0700

    added simple logging and default port of 8000

--- main.go
+++ main.go
@@ -10,16 +10,19 @@ import (
 var mut = sync.Mutex{}
 
 func main() {
-
 	fileserver := http.FileServer(http.Dir("./"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    // TODO: log like python exactly?
+    // 127.0.0.1 - - [13/Oct/2017 02:18:04] "GET /main.go HTTP/1.1" 200 -
+    log.Println(r.Method, r.URL.Path)
 		w.Header().Add("cache-control", "no-cache")
 		fileserver.ServeHTTP(w, r)
 	})
 
-  port := ":8000"
+  port := "8000"
   if len(os.Args) > 1 {
-    port = ":"+os.Args[1]
+    port = os.Args[1]
   }
-  log.Fatal(http.ListenAndServe(port, nil))
+  log.Println("Serving HTTP on 0.0.0.0 port " + port)
+  log.Fatal(http.ListenAndServe(":"+port, nil))
 }
